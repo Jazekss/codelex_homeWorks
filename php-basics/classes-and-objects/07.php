b8f07c94b8d4efa5b638a25aa681c450e574bc0a
Jazeks <89478349+Jazekss@users.noreply.github.com>
2022-02-04 18:34:45 +0200

    Update 07.php

--- php-basics/classes-and-objects/07.php
+++ php-basics/classes-and-objects/07.php
@@ -3,36 +3,58 @@ class Dog
 {
 	private string $name;
 	private string $sex;
-	private array $parents;
-	public function __construct(string $name, string $sex){
+	private string $mother;
+	private string $father;
+	public function __construct(string $name, string $sex, string $mother = "Unknown", string $father = "Unknown"){
 		$this->name = $name;
 		$this->sex = $sex;
+		$this->mother = $mother;
+		$this->father = $father;
 	}
-	public function setParens(){
-
-	}
-}
-class DogsTest extends Dog
-{
-	public function getName(string $name){
+	public function getName(): string{
 		return $this->name;
 	}
-	public function getSex(string $sex){
-		return $sex;
+	public function getSex(): string{
+		return $this->sex;
+	}
+	public function getMother(): string{
+		return $this->mother;
+	}
+	public function getFather(): string {
+		return $this->father;
 	}
 }
-$dogs = new DogsTest;
-$max = new Dog("Max", "male");
-$rocky = new Dog("Rocky", "male");
-$sparky = new Dog("Sparky", "male");
-$buster = new Dog("Buster", "male");
-$sam = new Dog("Sam", "male");
-$lady = new Dog("Lady", "female");
-$molly = new Dog("Molly", "female");
+class DogsTest
+{
+	private array $dogs;
+	public function __construct( array $dogs){
+		$this->dogs = $dogs;
+	}
+	public function isMother(Dog $dogs): bool {
+		if ($dogs->getMother() === $dogs->getMother()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+$dogs[] = new Dog("Max", "male", "Lady", "Rocky");
+$dogs[] = new Dog("Rocky", "male", "Molly", "Sam");
+$dogs[] = new Dog("Sparky", "male");
+$dogs[] = new Dog("Buster", "male", "Lady", "Rocky");
+$dogs[] = new Dog("Sam", "male");
+$dogs[] = new Dog("Lady", "female");
+$dogs[] = new Dog("Molly", "female");
+$dogs[] = new Dog("Coco", "female", "Lady", "Rocky");
 
-
-die;
-
-foreach($dog->getName() as $index => $product){
-	echo "= [{$index}] {$dog->getName()} - {$dog->getSex()}\n";
+foreach($dogs as $dog){
+	echo "\e[0;32m{$dog->getName()}\e[0m has mother \e[0;32m{$dog->getMother()}\e[0m and father \e[0;32m{$dog->getFather()}\e[0m \n";
+}
+echo "Dogs has same mothers: ";
+foreach($dogs as $index => $dog){
+	$same[$index] = $dog->getMother();
+	$same2[$index] = $dog->getMother();
+	if($same[$index] == $same2[$index]){
+		echo "{$same[$index]} \n";
+	}
 }
