6f557a57ac2e71271e19d1e11555c2340ed79630
Jazeks <89478349+Jazekss@users.noreply.github.com>
2022-02-09 17:35:50 +0200

    Update 02.php

--- php-basics/classes-and-objects/02.php
+++ php-basics/classes-and-objects/02.php
@@ -1,26 +1,26 @@
 <?php
 class Point
 {
-	public array $p1;
-	public array $p2;
-	public function __construct($p1, $p2){
+	private int $p1;
+	private int $p2;
+	public function __construct(int $p1, int $p2)	{
 		$this->p1 = $p1;
 		$this->p2 = $p2;
 	}
-	public function swapPoints($p1, $p2){
-		$p3 = '';
-		$p3 = $p1;
-		$p1 = $p2;
-		$p2 = $p3;
-		return $p1 . ", " . $p2;
+	public function swapPoints(Point &$a, Point &$b): void {
+		$temp = $a;
+		$a = $b;
+		$b = $temp;
+	}
+	public function returnA(): int {
+		return $this->p1;
+	}
+	public function returnB(): int {
+		return $this->p2;
 	}
 }
-$p1 = new Point(5, 2);
-$p2 = new Point(-3, 6);
-$swap->swapPoints($p1, $p2);
-echo "P1 = ({$p1[0]}, {$p1[1]}) and P2 = ({$p2[0]}, {$p2[1]})\n";
-
-
-
-//$p11 = $swap->swapPoints($p1, $p2);
-//print_r($p11[0]);
+$first = new Point(5, 2);
+$second = new Point(-3, 6);
+$first->swapPoints($first, $second);
+echo "({$first->returnA()}, {$first->returnB()})\n";
+echo "({$second->returnA()}, {$second->returnB()})";
